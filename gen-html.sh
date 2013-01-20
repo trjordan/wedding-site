ec7ad247da2cbeab69fac7b5d7190afe9cfb766e
TR Jordan <tr@tracelytics.com>
2013-01-20 14:22:31 -0500

    Fix egregious find-replace error.

--- gen-html.sh
+++ gen-html.sh
@@ -8,22 +8,22 @@ sed -i '' 's|img/|../img/|g' directions/index.html
 sed -i '' 's|css/|../css/|g' directions/index.html
 
 cp index.html event/index.html
-sed -i '' 's/" data-event="home"/ alt" data-event="home"/g' event/index.html
-sed -i '' 's/ alt" data-event="event"/" data-event="event"/g' event/index.html
+sed -i '' 's/" data-location="home"/ alt" data-location="home"/g' event/index.html
+sed -i '' 's/ alt" data-location="event"/" data-location="event"/g' event/index.html
 sed -i '' 's|js/|../js/|g' event/index.html
 sed -i '' 's|img/|../img/|g' event/index.html
 sed -i '' 's|css/|../css/|g' event/index.html
 
 cp index.html fun-stuff/index.html
-sed -i '' 's/" data-fun-stuff="home"/ alt" data-fun-stuff="home"/g' fun-stuff/index.html
-sed -i '' 's/ alt" data-fun-stuff="fun-stuff"/" data-fun-stuff="fun-stuff"/g' fun-stuff/index.html
+sed -i '' 's/" data-location="home"/ alt" data-location="home"/g' fun-stuff/index.html
+sed -i '' 's/ alt" data-location="fun-stuff"/" data-location="fun-stuff"/g' fun-stuff/index.html
 sed -i '' 's|js/|../js/|g' fun-stuff/index.html
 sed -i '' 's|img/|../img/|g' fun-stuff/index.html
 sed -i '' 's|css/|../css/|g' fun-stuff/index.html
 
 cp index.html story/index.html
-sed -i '' 's/" data-story="home"/ alt" data-story="home"/g' story/index.html
-sed -i '' 's/ alt" data-story="story"/" data-story="story"/g' story/index.html
+sed -i '' 's/" data-location="home"/ alt" data-location="home"/g' story/index.html
+sed -i '' 's/ alt" data-location="story"/" data-location="story"/g' story/index.html
 sed -i '' 's|js/|../js/|g' story/index.html
 sed -i '' 's|img/|../img/|g' story/index.html
 sed -i '' 's|css/|../css/|g' story/index.html
